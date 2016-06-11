08441b966d8cf612d1cc65cf5e129b970877e706
Randy Apuzzo <apuzzo@gmail.com>
2016-06-10 22:44:19 -0700

    removed cookies for local storage

--- src/js/controllers/signup.js
+++ src/js/controllers/signup.js
@@ -6,17 +6,42 @@ app.controller('SignupFormController', ['$scope', '$http', '$state', function($s
     $scope.authError = null;
     $scope.signup = function() {
       $scope.authError = null;
-      // Try to create
-      $http.post('api/signup', {name: $scope.user.name, email: $scope.user.email, password: $scope.user.password})
-      .then(function(response) {
-        if ( !response.data.user ) {
-          $scope.authError = response;
-        }else{
-          $state.go('app.dashboard-v1');
-        }
-      }, function(x) {
-        $scope.authError = 'Server Error';
-      });
+      // Try to create new user
+        $http({
+            url: 'https://www.echomtg.com/api/user/register/',
+            method: "POST",
+            params: {
+                email: $scope.user.email,
+                password: $scope.user.password,
+                username: $scope.user.username,
+                app: 'sleeveup'
+            }
+        }) .then(function(response) {
+            if (response.data && response.data.token) {
+                var token = response.data.token;
+                $http({
+                    url: 'https://www.echomtg.com/api/user/meta/',
+                    method: "GET",
+                    params: {
+                        auth: token
+                    }
+                })
+                    .then(function(response) {
+                        setCookie(token, response.data.user);
+                        $state.go('app.dashboard-v1');
+                    })
+            }
+        }, function(response) {
+            $scope.authError = 'Server Error';
+        });
     };
+
+    var setCookie = function (token, meta) {
+        $cookies.put('token', token);
+        for (var key in meta) {
+            $cookies.put(key, meta[key]);
+        }
+    };
+
   }])
  ;
